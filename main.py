21f7967f25ee3d5570bbbeffcf9cda0f690b34f1
VictorMartinCobos <victormartincobos@gmail.com>
2022-01-07 21:24:53 +0100

    Fixed calls to functions from others modules.

--- main.py
+++ main.py
@@ -7,10 +7,9 @@ api_aemet = os.environ.get("api_aemet")
 mongo_uri = os.environ.get("mongo_uri")
 
 def getTemperatures(nombreMunicipio, api_key_aemet, mongo_string):
-  jsonMunicipios = getJsonMunicipios(api_key_aemet)
-  idMunicipio = getIdMunicipio(nombreMunicipio,jsonMunicipios)
-  temperaturesForecast = getTemperatureForecast(nombreMunicipio, idMunicipio, api_key_aemet)
-  insertDataMongo(mongo_string, temperaturesForecast)
+  jsonMunicipios = aemet.getJsonMunicipios(api_key_aemet)
+  idMunicipio = aemet.getIdMunicipio(nombreMunicipio,jsonMunicipios)
+  temperaturesForecast = aemet.getTemperatureForecast(nombreMunicipio, idMunicipio, api_key_aemet)
+  mongo.insertDataMongo(mongo_string, temperaturesForecast)
   return(temperaturesForecast)
-  
-  
+
